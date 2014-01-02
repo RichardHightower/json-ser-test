77e67de2eab3a9133a3c62cf689209b996b015ed
Richard Hightower <richardhightower@gmail.com>
2014-01-02 04:10:49 -0800

    still faster

--- src/main/java/org/bura/jsonser/BoonJavaSerializerImpl.java
+++ src/main/java/org/bura/jsonser/BoonJavaSerializerImpl.java
@@ -2,13 +2,13 @@ package org.bura.jsonser;
 
 
 import org.boon.json.JsonSerializer;
-import org.boon.json.implementation.JsonSimpleSerializerImpl;
+import org.boon.json.JsonSerializerFactory;
 
 /**
  * Created by rick on 12/29/13.
  */
 public class BoonJavaSerializerImpl implements DataSerializer {
-    JsonSerializer serializer = new JsonSimpleSerializerImpl ();
+    JsonSerializer serializer = new JsonSerializerFactory().useAnnotations ().create ();
 
     @Override
     public Object serialize ( Object data ) {
